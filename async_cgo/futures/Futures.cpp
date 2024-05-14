7d8dc2789e6b76bda783790e4bf9c54f45d260ac
chyezh <chyezh@outlook.com>
2024-05-14 17:30:35 +0800

    feature: single callback

--- async_cgo/futures/Futures.cpp
+++ async_cgo/futures/Futures.cpp
@@ -3,25 +3,34 @@
 #include <folly/executors/ThreadedExecutor.h>
 #include <folly/futures/Future.h>
 
+#include <chrono>
 #include <iostream>
 #include <string>
-
-int* foo() {
-  int* x = new int{10076};
-  // do something with x
-  std::cout << "foo(" << *x << ")" << std::endl;
-  throw std::runtime_error("foo error");
-}
+#include <thread>
 
 int main() {
-  IFuture* future = new Future<int*, std::function<int*()>>(foo);
+  auto future = new Future<int*>();
 
-  future->run();
-  // future->cancel(folly::FutureCancellation());
-  try {
-    auto result = future->get();
-    std::cout << "result: " << *static_cast<int*>(result) << std::endl;
-  } catch (const std::exception& e) {
-    std::cout << "exception: " << e.what() << std::endl;
-  }
+  future->asyncProduce([]() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    int* x = new int{10076};
+    // do something with x
+    std::cout << "foo(" << *x << ")" << std::endl;
+    return x;
+  });
+
+  future->asyncConsume(
+      [](int* x) { std::cout << "consume(" << *x << ")" << std::endl; },
+      [](std::exception& e) {
+        std::cout << "exception: " << e.what() << std::endl;
+      });
+
+  future->cancel(folly::FutureCancellation());
+
+  std::cout << "main thread is running..." << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  std::cout << "main thread is done..." << std::endl;
+
+  delete future;
+  return 0;
 }
--- async_cgo/futures/Futures.h
+++ async_cgo/futures/Futures.h
@@ -8,21 +8,20 @@
 #include <memory>
 #include <variant>
 
+template <typename R>
+using Contract = std::pair<folly::Promise<R>, folly::SemiFuture<R>>;
+
+template <typename R>
+using ContractPtr = std::shared_ptr<Contract<R>>;
+
 /// @brief a virtual class that represents a future that can be run,
 /// cancelled, and get.
 class IFuture {
  public:
-  /// @brief  the wrapped function and sets the result of the future.
-  virtual void run() = 0;
-
   /// @brief cancel the future with the given exception,
   /// and the get method
   /// throws a folly::FutureException .
   virtual void cancel(folly::FutureException&& e) = 0;
-
-  /// @brief block and get the result of the future, the type is determined by
-  /// the implementation.
-  virtual void* get() = 0;
 };
 
 /// @brief  Future is a implementation of IFuture.
@@ -30,36 +29,81 @@ class IFuture {
 /// pointer.
 /// @tparam Fn is the type of the wrapped function, which must be nothrow and
 /// return a R type.
-template <class R, class Fn, typename = std::enable_if<std::is_pointer_v<R>>,
-          typename = std::enable_if<std::is_invocable_r_v<R, Fn>>>
+template <class R, typename = std::enable_if<std::is_pointer_v<R>>>
 class Future : public IFuture {
  public:
-  Future(Fn&& fn)
-      : promise_(), future_(promise_.getSemiFuture()), fn_(std::move(fn)) {
-    promise_.setInterruptHandler(
-        [this](auto&& e) { promise_.setException(e); });
+  Future(folly::Executor::KeepAlive<> executor = folly::getGlobalCPUExecutor(),
+         int8_t priority = 0) noexcept
+      : contract_(
+            std::make_shared<Contract<R>>(folly::makePromiseContract<R>())),
+        executor_(executor),
+        priority_(priority) {}
+
+  Future(const Future&) = delete;
+
+  Future(const Future&&) = delete;
+
+  template <typename Fn, typename... Args,
+            typename = std::enable_if<std::is_invocable_r_v<R, Fn, Args...>>>
+  void asyncProduce(Fn&& fn, Args&&... args) noexcept {
+    asyncProduce(executor_, priority_, std::forward<Fn>(fn),
+                 std::forward<Args>(args)...);
   }
 
-  /// run the wrapped function and set the result of the future.
-  void run() noexcept {
-    try {
-      promise_.setValue(fn_());
-    } catch (const std::exception& e) {
-      promise_.setException(e);
-    }
+  template <typename Fn, typename... Args,
+            typename = std::enable_if<std::is_invocable_r_v<R, Fn, Args...>>>
+  void asyncProduce(folly::Executor::KeepAlive<> executor, int8_t priority,
+                    Fn&& fn, Args&&... args) noexcept {
+    // initialize the interrupt handler for the promise of contract.
+    auto interrupt_handler = [contract = contract_](auto& e) {
+      // just raise the exception to the semi future if future sent the
+      // interrupt signal.
+      contract->first.setException(std::move(e));
+    };
+    contract_->first.setInterruptHandler(std::move(interrupt_handler));
+
+    // start produce process async.
+    auto runner = [fn = std::move(fn), &args...]() {
+      return fn(std::forward<Args>(args)...);
+    };
+    auto thenRunner = [contract = contract_,
+                       runner = std::move(runner)](auto&&) {
+      contract->first.setWith(std::move(runner));
+    };
+    folly::makeSemiFuture().via(executor, priority).then(thenRunner);
   }
 
-  void cancel(folly::FutureException&& e) noexcept { future_.raise(e); }
+  template <typename RFn, typename EFn,
+            typename = std::enable_if<std::is_invocable_v<RFn, R>>,
+            typename =
+                std::enable_if<std::is_invocable_v<EFn, const std::exception&>>>
+  void asyncConsume(RFn&& rfn, EFn&& efn) {
+    asyncConsume(executor_, priority_, std::forward<RFn>(rfn),
+                 std::forward<EFn>(efn));
+  }
 
-  void* get() noexcept { return static_cast<void*>(std::move(future_).get()); }
+  template <typename RFn, typename EFn,
+            typename = std::enable_if<std::is_invocable_v<RFn, R>>,
+            typename =
+                std::enable_if<std::is_invocable_v<EFn, const std::exception&>>>
+  void asyncConsume(folly::Executor::KeepAlive<> executor, int8_t priority,
+                    RFn&& rfn, EFn&& efn) {
+    std::move(contract_->second)
+        .via(executor, priority)
+        .thenValue([contract = contract_, rfn = std::move(rfn)](auto&& result) {
+          rfn(std::move(result));
+        })
+        .thenError(
+            folly::tag_t<std::exception>{},
+            [contract = contract_, efn = std::move(efn)](auto&& e) { efn(e); });
+  }
+
+  void cancel(folly::FutureException&& e) noexcept {
+    contract_->second.raise(e);
+  }
 
  private:
-  Fn fn_;
-  folly::Promise<R> promise_;
-  folly::SemiFuture<R> future_;
+  ContractPtr<R> contract_;
+  folly::Executor::KeepAlive<> executor_;
+  int8_t priority_;
 };
-
-template <class R, class Fn>
-std::unique_ptr<IFuture> CreateFuture(Fn&& fn) {
-  return std::make_unique<Future<R, Fn>>(std::forward<Fn>(fn));
-}
